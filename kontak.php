a739570a6a83c293a96de6dff6f6ce0cbb9d7631
krishnaar <krishnaalrasyid@gmail,com>
2017-01-24 12:05:58 +0700

    penambahan kontak

---
+++ kontak.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ini kontak</title>
+</head>
+<body>
+	ini kontak loh...!!!
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ini profile</title>
+</head>
+<body>
+	<h1>ini profile</h1>
+</body>
+</html>
